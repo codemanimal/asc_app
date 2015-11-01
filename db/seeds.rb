d70c4a38bd30603642c15d70d2eda116ebd77752
Mark Smukler <smukler.mark@gmail.com>
2015-11-01 11:34:07 -0500

    seeded database with test brands

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Destroys all current brokers
+Brand.destroy_all
+
+brand_data = [
+	{
+	:name=>"La Esquina Corner Deli",
+	:rating=>4.5,
+	:is_card=>true,
+	:is_available=>true
+	},
+	{
+	:name=>"Lure Fishbar",
+	:rating=>4.5,
+	:is_card=>true,
+	:is_available=>true
+	},
+	{
+	:name=>"Marea",
+	:rating=>4.5,
+	:is_card=>true,
+	:is_available=>true
+	},
+	{
+	:name=>"Balthazar Restaurant",
+	:rating=>4.5,
+	:is_card=>true,
+	:is_available=>true
+	},
+	{
+	:name=>"The Meatball Shop",
+	:rating=>4.5,
+	:is_card=>true,
+	:is_available=>true
+	}
+]
+
+brand_data.each do |brand|
+  Brand.create!({
+    name: brand[:name],
+    rating: brand[:rating],
+    is_card: brand[:is_card],
+    is_available: brand[:is_available]
+  })
+end
