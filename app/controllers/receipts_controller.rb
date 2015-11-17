01d839859e054fa1ab19662cd55bec9c184162eb
Mark Smukler <smukler.mark@gmail.com>
2015-11-17 15:54:40 -0500

    edits to automation algorithm complete

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -8,8 +8,7 @@ class ReceiptsController < ApplicationController
 		# Create a new receipt entry
 		@receipt = Receipt.new(receipt_params)
 		# Take the new entries name and find it in YELP
-		merchant_name = @receipt.merchant
-		params = { term: merchant_name }
+		params = { term: @receipt.merchant }
 		# Hit YELP API
 		brands_data = Yelp.client.search('New York', params)
 		# Save key data points from YELP results
@@ -17,7 +16,7 @@ class ReceiptsController < ApplicationController
 		brand_rating = brands_data.businesses.first.rating
 		brand_yelp_id = brands_data.businesses.first.id
 		brand_category = brands_data.businesses.first.categories.first.second
-		brand_category_id = Category.where(subcategories.include?brand_category).id
+		
 		# Check to see if it has a card and is available in cards (brands) table
 		if Brand.all.find_by name: brand_name
 			# If it exists, set it to a variable 
@@ -32,64 +31,79 @@ class ReceiptsController < ApplicationController
 				# find all animals in that category
 				animal_entries = Category.where(business_type: category_type)
 				# find the correct animal using ranges
-				if animal_entries[0].price_floor.include?(@receipt.amount.to_s)
-					animal = animal_entries[0].animal.sample
-					print "Use this animal: " + animal + "\n"
-					redirect_to root_path
-				elsif animal_entries[1].price_floor.include?(@receipt.amount.to_s)
-					animal = animal_entries[1].animal.sample
-					print "Use this animal: " + animal + "\n"
-					redirect_to root_path
-				else animal_entries[2].price_floor.include?(@receipt.amount.to_s)
+				if animal_entries[2].price_floor <= @receipt.amount
 					animal = animal_entries[2].animal.sample
 					print "Use this animal: " + animal + "\n"
-					redirect_to root_path
-				end
+					redirect_to root_path and return
+				elsif animal_entries[1].price_floor <= @receipt.amount
+					animal = animal_entries[1].animal.sample
+					print "Use this animal: " + animal + "\n"
+					redirect_to root_path and return
+				else 
+					animal = animal_entries[0].animal.sample
+					print "Use this animal: " + animal + "\n"
+					redirect_to root_path and return
+				end				
 			else
-				# if the brand is not available, find brand with same rating  and category ID and use that brand
-				selected_brand = Brand.where(rating: brand_rating, category_id: brand_category_id,  is_card: true, is_available:true)
-				print "Use this brand: " + selected_brand.sample.name + "\n"
-				# same as above
-				category = Category.find_by id: selected_brand.category_id
-				category_type = category.business_type
-				animal_entries = Category.where(business_type: category_type)
-				if animal_entries[0].price_floor.include?(@receipt.amount.to_s)
-					animal = animal_entries[0].animal.sample
-					print "Use this animal: " + animal + "\n"
-					redirect_to root_path
-				elsif animal_entries[1].price_floor.include?(@receipt.amount.to_s)
-					animal = animal_entries[1].animal.sample
-					print "Use this animal: " + animal + "\n"
-					redirect_to root_path
-				else animal_entries[2].price_floor.include?(@receipt.amount.to_s)
-					animal = animal_entries[2].animal.sample
-					print "Use this animal: " + animal + "\n"
-					redirect_to root_path
-				end
+				Category.all.each do |category|
+					if category.subcategories.include?brand_category
+						# if the brand is not available, find brand with same rating  and category ID and use that brand
+						selected_brand = Brand.where(rating: brand_rating, category_id: category.id, is_available:true).sample
+						selected_brand_name = selected_brand.name
+						binding.pry
+						print "Use this brand: " + selected_brand_name + "\n"
+						# Same as above
+						category = Category.find_by id: category.id
+						category_type = category.business_type
+						animal_entries = Category.where(business_type: category_type)
+						if animal_entries[2].price_floor <= @receipt.amount
+							animal = animal_entries[2].animal.sample
+							print "Use this animal: " + animal + "\n"
+							redirect_to root_path and return
+						elsif animal_entries[1].price_floor <= @receipt.amount
+							animal = animal_entries[1].animal.sample
+							print "Use this animal: " + animal + "\n"
+							redirect_to root_path and return
+						else 
+							animal = animal_entries[0].animal.sample
+							print "Use this animal: " + animal + "\n"
+							redirect_to root_path and return
+						end
+						return selected_brand_name + " & " + animal
+					end
+				end	
 			end
 		else
-			# if the brand is not available, find brand with same rating  and category ID and use that brand
-			selected_brand = Brand.where(rating: brand_rating, category_id: brand_category_id,  is_card: true, is_available:true)
-			print "Use this brand: " + selected_brand.sample.name + "\n"
-			# Same as above
-			category = Category.find_by id: selected_brand.category_id
-			category_type = category.business_type
-			animal_entries = Category.where(business_type: category_type)
-			if animal_entries[0].price_floor.include?(@receipt.amount.to_s)
-				animal = animal_entries[0].animal.sample
-				print "Use this animal: " + animal + "\n"
-				redirect_to root_path
-			elsif animal_entries[1].price_floor.include?(@receipt.amount.to_s)
-				animal = animal_entries[1].animal.sample
-				print "Use this animal: " + animal + "\n"
-				redirect_to root_path
-			else animal_entries[2].price_floor.include?(@receipt.amount.to_s)
-				animal = animal_entries[2].animal.sample
-				print "Use this animal: " + animal + "\n"
-				redirect_to root_path
-			end
+			Category.all.each do |category|
+				if category.subcategories.include?brand_category
+					# if the brand is not available, find brand with same rating  and category ID and use that brand
+					selected_brand = Brand.where(rating: brand_rating, category_id: category.id, is_available:true).sample
+					binding.pry
+					selected_brand_name = selected_brand.name
+					print "Use this brand: " + selected_brand_name + "\n"
+					# Same as above
+					category = Category.find_by id: category.id
+					category_type = category.business_type
+					animal_entries = Category.where(business_type: category_type)
+					if animal_entries[2].price_floor <= @receipt.amount
+						animal = animal_entries[2].animal.sample
+						print "Use this animal: " + animal + "\n"
+						redirect_to root_path and return
+					elsif animal_entries[1].price_floor <= @receipt.amount
+						animal = animal_entries[1].animal.sample
+						print "Use this animal: " + animal + "\n"
+						redirect_to root_path and return
+					else 
+						animal = animal_entries[0].animal.sample
+						print "Use this animal: " + animal + "\n"
+						redirect_to root_path and return
+					end
+					return selected_brand_name + " & " + animal
+				end
+			end	
 		end
 	end
+
 
 	private
 
